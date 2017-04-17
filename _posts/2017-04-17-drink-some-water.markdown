70535d6a148a1bee9f6ec3bcf6bf376be52343c9
Harry <arandomawesomegeek@gmail.com>
2017-04-17 11:25:17 -0400

    turn about into an object

---
+++ _posts/2017-04-17-drink-some-water.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title: "Drink Some Water"
+date: 2017-04-17 11:10:44 -0400
+categories: web javascript
+summary: "Tells you when you need to drink water. Website version"
+link: "https://jli.host/water/water.html"
+desc: "A little web app thing that reminds you to drink water using the Notification API"
+---
--- js/about.js
+++ js/about.js
@@ -1,23 +1,23 @@
 harry = require("home.js");
 
-harry.about = [];
+harry.about = {};
 
-harry.about.append({"awards": [{"MATHCOUNTS 2013": "State 2nd Place Individual, 1st Place Team"},
-                               {"VEX Robotics 2017": "State Tournament Winner"},
-                               {"Philadelphia Classic 2016": "First Place Novice Team"}]});
+harry.about.awards = [{"MATHCOUNTS 2013": "State 2nd Place Individual, 1st Place Team"},
+                      {"VEX Robotics 2017": "State Tournament Winner"},
+                      {"Philadelphia Classic 2016": "First Place Novice Team"}];
 
-harry.about.append({"likes": ["Linux",
-                              "Web Development",
-                              "Deep Learning",
-                              "Networking"]});
+harry.about.likes = ["Linux",
+                     "Web Development",
+                     "Deep Learning",
+                     "Networking"];
 
-harry.about.append({"skills": ["Redis",
-                               "MongoDB",
-                               "TensorFlow",
-                               "NodeJS",
-                               "C++",
-                               "Java",
-                               "AngularJS 1+2",
-                               "Linux"]});
+harry.about.skills = ["Redis",
+                      "MongoDB",
+                      "TensorFlow",
+                      "NodeJS",
+                      "C++",
+                      "Java",
+                      "AngularJS 1+2",
+                      "Linux"];
 
 harry.about.append({"resume": "in pdf"});
